edab2a48b5775e31bb15e8245faeb3bbc17a3d6c
Agnes Leady O <35962458+leadymunte@users.noreply.github.com>
2018-04-02 00:41:43 +0700

    Create insert.1301160063.php

---
+++ WEPRO/tpModul7/insert.1301160063.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>FORM DAFTAR</title>
+</head>
+<body>
+	<H3>Daftar Baru</H3>
+	<form method="post" action="daftar_1301160063.php">
+		nim  : <input type="text" name="nim"/><br>
+		nama : <input type="text" name="nama"/><br>
+		<input type="submit" value="daftar"/>		
+	</form>
+
+</body>
+</html>
